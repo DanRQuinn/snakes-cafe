4f0684ae895fccda9b2cb6bc8fe3f1f6492a4089
Daniel Quinn <dandidit.quinn@gmail.com>
2023-06-12 19:50:12 -0700

    works

---
+++ snakes_cafe.py
@@ -0,0 +1,57 @@
+def menu():
+    menu = """
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************"""
+
+print(menu)
+
+
+def order():
+    orders = {}
+    while True:
+        order = input(">")
+        if order == "quit":
+            break
+        if order in orders:
+            orders[order] += 1
+        else:
+            orders[order] = 1
+        print(f"\n** Thanks for ordering! {orders[order]} order(s) of {order} will be right up! **\n")
+    print("Exiting")
+
+if __name__ == "__main__":
+    menu()
+    order()
